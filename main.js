45ec9e4b8c7cd3baa3ad0d6747cc35e0d2ea4076
Goro <misionut13@gmail.com>
2024-03-27 15:03:16 +0200

    The files i haven't used since the heian era

---
+++ main.js
@@ -0,0 +1,14 @@
+
+//navbar
+function hideiconbar(){
+    var baricon = document.getElementById("baricon")
+    var navigation = document.getElementById("navigation")
+    baricon.setAttribute("style", "display:none;");
+    navigation.classList.remove("hide");
+}
+function showiconbar(){
+    var baricon = document.getElementById("baricon")
+    var navigation = document.getElementById("navigation")
+    baricon.setAttribute("style", "display:block;");
+    navigation.classList.add("hide");
+}
